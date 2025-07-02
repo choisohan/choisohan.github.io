7c02687a198732947ad9acfa85f9ece2e1d01fc2
Min <55426737+choisohan@users.noreply.github.com>
2025-07-01 17:38:37 -0700

    reactNode import2

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import type { ReactNode } from 'react';
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
